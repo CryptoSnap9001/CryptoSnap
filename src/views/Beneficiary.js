60d55d784247bdf74788d49ebad984d0baf5d878
einsteinder <einsteinder@gmail.com>
2018-04-22 11:20:19 -0400

    add balace

--- src/views/Beneficiary.js
+++ src/views/Beneficiary.js
@@ -4,10 +4,27 @@ import {connect} from 'react-redux'
 var QRCode = require('qrcode.react');
 
 class Beneficiary extends Component {
+  state={
+    balance:""
+  }
+  componentDidMount(){
+    fetch("https://us-central1-cryptosnap-9001.cloudfunctions.net/balance",{
+      body: JSON.stringify({ "user_id": this.props.user.uid }),
+      method:'POST',
+      mode:'cors',
+      headers:{
+        'content-type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => this.setState({ balance: data.balance }));
+
+  }
   render() {
     return (
       <div>
         <h1>Beneficiary Account!!!</h1>
+        <p>Balance:{this.state.balance}</p>
         <QRCode value={this.props.user.uid} />,
 
       </div>
